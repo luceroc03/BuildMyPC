b8522650d48aba8ded9394bfdee492eff0333ac3
Lucero Cervantes <156225314+luceroc03@users.noreply.github.com>
2024-02-06 10:28:32 -0500

    Update Dimension.java

--- src/BuildMyPc/Dimension.java
+++ src/BuildMyPc/Dimension.java
@@ -7,11 +7,32 @@ public class Dimension {
     private double depth;
 
     public Dimension(double height, double width, double depth){
-        this.depth;
-        this.height;
-        this.width;
+        this.depth = depth;
+        this.height = height;
+        this.width = width;
     }
 
+    public double getHeight() {
+        return height;
+    }
 
+    public void setHeight(double height) {
+        this.height = height;
+    }
 
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getDepth() {
+        return depth;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
 }
