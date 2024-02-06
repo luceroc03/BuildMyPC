b61cf39551621b3b354564e492a7ae690535135c
Lucero Cervantes <156225314+luceroc03@users.noreply.github.com>
2024-02-06 10:29:13 -0500

    Update Monitor.java

--- src/BuildMyPc/Monitor.java
+++ src/BuildMyPc/Monitor.java
@@ -1,5 +1,12 @@
 package BuildMyPc;
-
+/**
+ * Acer - Nitro XZ320Q P3bmiiphx 31.5”
+ * LED Curved FHD FreeSync Premium Monitor - Black
+ * Model:XZ320Q P3bmiiphxSKU:6538259
+ * maximum resolution: 1920 * 1080
+ * screen size 31.5 inches
+ * refrehs rate is 165Hz
+ */
 public class Monitor {
     private String manufacturer;
     private String model;
@@ -13,6 +20,46 @@ public class Monitor {
         this.model = model;
         this.resolution = resolution;
         this.screenSize = screenSize;
+        this.refreshRate = refreshRate;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Resolution getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(Resolution resolution) {
+        this.resolution = resolution;
+    }
+
+    public double getScreenSize() {
+        return screenSize;
+    }
+
+    public void setScreenSize(double screenSize) {
+        this.screenSize = screenSize;
+    }
+
+    public int getRefreshRate() {
+        return refreshRate;
+    }
+
+    public void setRefreshRate(int refreshRate) {
         this.refreshRate = refreshRate;
     }
 }
