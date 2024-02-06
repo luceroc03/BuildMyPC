08ffd38d90706db8c34095e36e28c387b8934260
Lucero Cervantes <156225314+luceroc03@users.noreply.github.com>
2024-02-06 10:31:14 -0500

    Update Resolution.java

--- src/BuildMyPc/Resolution.java
+++ src/BuildMyPc/Resolution.java
@@ -9,4 +9,19 @@ public class Resolution {
         this.width = width;
     }
 
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
 }
